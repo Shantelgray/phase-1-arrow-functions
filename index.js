d4fd183835f57f54981ef8390f53a35d180198a6
Shantel <Shantelgray24@gmail.com>
2025-05-08 10:40:19 -0400

    Creating arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+let divide = (a, b) => {
+  let reuslt = a / b;
+  return reuslt;
+}
+let square = n => n * 2;
+  alert( square(2 ));
+
+  const add = (a,b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+  }
+  
